63841b4990219facba35b1007fe56ef11d5d3749
AndriCaba <106049169+AndriCaba@users.noreply.github.com>
2024-08-26 00:56:08 +0800

    Update get_leaderboard.php

--- get_leaderboard.php
+++ get_leaderboard.php
@@ -22,7 +22,7 @@ $resultSections = sqlsrv_query($conn, $sqlSections);
 $sections = array();
 if ($resultSections) {
     while($row = sqlsrv_fetch_array($resultSections, SQLSRV_FETCH_ASSOC)) {
-        $sections[] = $row['Section'];
+        $sections[] = (string)$row['Section'];  // Casting as string
     }
 }
 
@@ -33,16 +33,17 @@ $resultDates = sqlsrv_query($conn, $sqlDates);
 $dates = array();
 if ($resultDates) {
     while($row = sqlsrv_fetch_array($resultDates, SQLSRV_FETCH_ASSOC)) {
-        $dates[] = $row['DateEnterLevel'];
+        $dates[] = (string)$row['DateEnterLevel'];  // Casting as string
     }
 }
 
 // SQL query to get leaderboard data
-$section = isset($_GET['section']) ? $_GET['section'] : '';
-$date = isset($_GET['date']) ? $_GET['date'] : '';
+$section = isset($_GET['section']) ? (string)$_GET['section'] : '';  // Casting as string
+$date = isset($_GET['date']) ? (string)$_GET['date'] : '';  // Casting as string
 
 $sql1 = "SELECT StudentID, LastName, Teacher, TopicLevel, DateEnterLevel, TimeEnterLevel, TimeRecord, Section FROM UserRecord";
 $conditions = array();
+$params = array();
 
 if ($section) {
     $conditions[] = "Section = ?";
@@ -63,7 +64,16 @@ $result1 = sqlsrv_query($conn, $sql1, $params);
 $userRecords = array();
 if ($result1) {
     while($row = sqlsrv_fetch_array($result1, SQLSRV_FETCH_ASSOC)) {
-        $userRecords[] = $row;
+        $userRecords[] = array(
+            "StudentID" => (int)$row['StudentID'],  // Casting as integer
+            "LastName" => (string)$row['LastName'],  // Casting as string
+            "Teacher" => (string)$row['Teacher'],  // Casting as string
+            "TopicLevel" => (string)$row['TopicLevel'],  // Casting as string
+            "DateEnterLevel" => (string)$row['DateEnterLevel'],  // Casting as string
+            "TimeEnterLevel" => (string)$row['TimeEnterLevel'],  // Casting as string
+            "TimeRecord" => (string)$row['TimeRecord'],  // Casting as string
+            "Section" => (string)$row['Section']  // Casting as string
+        );
     }
 }
 
